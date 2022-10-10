80c57d6c72884d5ff7d86ce81a890dfa7227f19a
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-10 16:30:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,22 @@ if len(num1) > len(num2):
 # Make your Code
 # ******************************
 
+sequenceCounter = 0
+
+for n in num2:
+    # checks if the subset is found or not
+    if sequenceCounter == len(num1):
+        break
+
+    if n == num1[sequenceCounter]:
+        sequenceCounter += 1
+    else:
+        sequenceCounter = 0
+
+
+if sequenceCounter == len(num1):
+    print(True)
+else:
+    print(False)
+    
 # print ('True') or ('False')
